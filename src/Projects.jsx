87a509aab90b4d30cbc6afccd5321d19e78edd0d
jmendez44 <jenuel.mendez@gmail.com>
2019-09-21 02:52:58 -0400

    project hover animation

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from "react";
+import { useSpring, animated } from "react-spring";
+
 import Routes from "./Routes";
 
 const Projects = () => {
   const [isToggled, setToggle] = useState(false);
+  const [isToggled1, setToggle1] = useState(false);
+  const [isToggled2, setToggle2] = useState(false);
+  const cardAni = useSpring({
+    transform: isToggled ? "translate(0,0)" : "translate(0,-100%)"
+  });
+  const cardAni1 = useSpring({
+    transform: isToggled1 ? "translate(0,0)" : "translate(0,-100%)"
+  });
+
+  const cardAni2 = useSpring({
+    transform: isToggled2 ? "translate(0,0)" : "translate(0,-100%)"
+  });
+
+  // const fade = useSpring({
+  //   from: {
+  //     opacity: 0
+  //   },
+  //   opacity: 1
+  // });
+  console.log(isToggled);
   return (
     <div className="projects-container">
       <video
@@ -13,30 +35,40 @@ const Projects = () => {
         src={require("./Assets/cloudy.mp4")}
       ></video>
       <div className="projects-wrapper">
-        <div className="title">
+        <div
+          onMouseEnter={() => setToggle(!isToggled)}
+          onMouseLeave={() => setToggle(!isToggled)}
+          className="title"
+        >
+          <animated.div style={cardAni} className="ani"></animated.div>
           <div className="project-card griipe">
             <img src={require("./Assets/2019-09-2002-02-59_Trim.gif")} />
           </div>
         </div>
-
-        <div className="title">
+        <div
+          onMouseEnter={() => setToggle1(!isToggled1)}
+          onMouseLeave={() => setToggle1(!isToggled1)}
+          className="title"
+        >
+          <animated.div style={cardAni1} className="ani1"></animated.div>
           <div className="project-card griipe">
             <img src={require("./Assets/cgol.gif")} />
           </div>
         </div>
 
-        <div className="title">
+        <div
+          onMouseEnter={() => setToggle2(!isToggled2)}
+          onMouseLeave={() => setToggle2(!isToggled2)}
+          className="title"
+        >
+          <animated.div style={cardAni2} className="ani2"></animated.div>
           <div className="project-card griipe">
             <img src={require("./Assets/Capture.PNG")} />
           </div>
         </div>
 
-        <div className="title">
-          
-          <div className="project-card griipe">
-            <img src="" />
-           
-          </div>
+        <div className="title more-info">
+          <div className="project-card more"></div>
         </div>
       </div>
     </div>
@@ -45,9 +77,8 @@ const Projects = () => {
 
 export default Projects;
 
-
-
-{/* <div className="title">
+{
+  /* <div className="title">
           <h2>More Projects</h2>
           <div className="project-card griipe">
             <img src="" />
@@ -61,4 +92,5 @@ export default Projects;
               <button>View Site</button>
               <button>Github</button>
             </div>
-          </div> */}
+          </div> */
+}

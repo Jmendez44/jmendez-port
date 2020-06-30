eb24919e0c740604a0acc0b6595890416d26601b
jmendez44 <jenuel.mendez@gmail.com>
2020-06-30 02:40:39 -0400

    resume change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Routes from "./Routes";
 const App = () => {
 
   const isMobile = window.innerWidth < 800;
-  const showItems = isMobile ? 1 : 3;
+  // const showItems = isMobile ? 1 : 3;
 
   return (
     <div className="App">
